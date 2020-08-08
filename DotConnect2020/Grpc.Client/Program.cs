5358e6ed2d0428db4d1828c5b43eeb7f8ea66053
Thai Anh Duc <anhduc.thai@gmail.com>
2020-08-08 21:01:25 +0700

    Add DotConnect2020 solution which contains stuff that I have tried in 2020, or maybe nothing at all

---
+++ DotConnect2020/Grpc.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
